426468da7184c0bf75274d58b230c4d380eafaf5
diegopaez03 <diegosan016@gmail.com>
2024-02-13 21:40:14 -0300

    se añadieron funcionalidades de docker(sin funcionar)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ScientistModule } from './scientist/scientist.module';
@@ -10,13 +11,14 @@ import { Creation } from './creation/entities/creation.entity';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      database: 'proyect',
-      host: 'localhost',
-      username: 'postgres',
-      password: 'password',
-      port: 5432,
+      database: process.env.PGDB_NAME,
+      host: process.env.PGDB_HOST,
+      username: process.env.PGDB_USERNAME,
+      password: process.env.PGDB_PASSWORD,
+      port: parseInt(process.env.PGDB_DOCKER_PORT, 10),
       entities: [Scientist, Creation],
       synchronize: true,
     }),
